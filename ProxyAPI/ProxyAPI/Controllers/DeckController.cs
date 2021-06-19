0eda210361cbe8a96922854dcfc1327d4ea36aca
jconnelly-dev <jconnelly@precisemrm.com>
2021-06-19 10:46:39 -0600

    upload endpoint is done

--- ProxyAPI/ProxyAPI/Controllers/DeckController.cs
+++ ProxyAPI/ProxyAPI/Controllers/DeckController.cs
@@ -15,6 +15,7 @@ using MagicConsumer;
 using MagicConsumer.WizardsAPI;
 using MagicConsumer.WizardsAPI.DTO;
 using System.IO;
+using System.Text;
 
 namespace ProxyAPI.Controllers
 {
@@ -29,6 +30,8 @@ namespace ProxyAPI.Controllers
     public class DeckController : ControllerBase
     {
         #region Members.
+        const string DECKLIST_FILENAME = "decklist.txt";
+
         private readonly int _trackingId;
         private readonly ProxyConfigs _configs;
         private readonly WizardsConsumer _consumer;
@@ -50,14 +53,18 @@ namespace ProxyAPI.Controllers
 
         #region Web API Endpoints.
         /// <summary>
-        /// Upload file containing deck list. Valid file line's begin w/a numeric, followed by whitespace, followed by the card name. i.e. "4 Liliana of the Veil".
+        /// Upload text file containing deck list.
         /// </summary>
         /// <returns>UploadIdentifier</returns>
+        /// <format>(card_quantity)(white_space)(card_name)</format>
+        /// <format_ex1>4 Ponder</format_ex1>
+        /// <format_ex2>2 Lightning Bolt</format_ex2>
+        /// <format_ex3>10 Swamp</format_ex3>
         /// <example>POST: /api/1.0/deck</example>
         /// <response code="200">Success</response>
-        /// <response code="400">Unable to get Upload file due to processing errors.</response>
-        /// <response code="500">Unable to get Upload file due to validation errors.</response>
-        [HttpPost("card/{name:minlength(1)}")]
+        /// <response code="400">Unable to get Upload File due to processing errors.</response>
+        /// <response code="500">Unable to get Upload File due to validation errors.</response>
+        [HttpPost()]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UploadIdentifier))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -73,6 +80,19 @@ namespace ProxyAPI.Controllers
                 return Problem(statusCode: StatusCodes.Status400BadRequest, detail: errMsg);
             }
 
+            // NOTE: I'll make this all into an Attribute later when I go through clean-up process.
+            // MIME Sniffing standards should be used here to enforce a bunch of security concerns w/reading in a file from the request.
+            string[] allowedFileTypes = new string[]
+            {
+                "text/plain"
+            };
+            if (decklist.Length <= 0 || decklist.ContentType == null || !allowedFileTypes.Contains(decklist.ContentType))
+            {
+                string errMsg = "Status 400 Bad Request: Invalid Request.";
+                _logger.Log(LogLevel.Error, eventId, errMsg);
+                return Problem(statusCode: StatusCodes.Status400BadRequest, detail: errMsg);
+            }
+
             UploadIdentifier result = null;
 
             try
@@ -80,6 +100,7 @@ namespace ProxyAPI.Controllers
                 string guid = null;
                 string decklistPath = null;
 
+                // Create a new/unique directory for this request that will store the decklist text file and all downloaded card images.
                 int currAttempt = 1;
                 int maxAttempts = 5;
                 bool uploadPathExists = false;
@@ -101,28 +122,24 @@ namespace ProxyAPI.Controllers
                 while (currAttempt <= maxAttempts);
                 if (!uploadPathExists || string.IsNullOrEmpty(guid) || string.IsNullOrEmpty(decklistPath))
                 {
-                    string errMsg = $"Status 500 Internal Server Error. Unable to Find Upload Pathing.";
+                    string errMsg = $"Status 500 Internal Server Error. Unable to Find Upload Decklist Pathing.";
                     _logger.Log(LogLevel.Error, eventId, errMsg);
                     return Problem(statusCode: StatusCodes.Status500InternalServerError, detail: errMsg);
                 }
 
-                // Copy the file to the server synchronously, waiting for it to finish.
-                string decklistFilePath = Path.Combine(decklistPath, guid);
-                using (FileStream fileStream = new FileStream(decklistFilePath, FileMode.Create))
-                {
-                    decklist.CopyTo(fileStream);
-                }
+                // Inside new request directory, create a file and synchronously copy to it the plain text contents from the request body file.
+                string decklistFilePath = Path.Combine(decklistPath, DECKLIST_FILENAME);
+                bool validCopy = SyncCopyValidPlainTextFileToFile(decklist, decklistFilePath);
 
                 // Determine if file was uploaded successfully.
-                if (!System.IO.File.Exists(decklistFilePath))
+                if (!validCopy || !System.IO.File.Exists(decklistFilePath))
                 {
-                    string errMsg = $"Status 500 Internal Server Error. Failed to Upload Decklist.";
+                    string errMsg = $"Status 500 Internal Server Error. Failed to Upload Decklist Text File.";
                     _logger.Log(LogLevel.Error, eventId, errMsg);
                     return Problem(statusCode: StatusCodes.Status500InternalServerError, detail: errMsg);
                 }
 
-                result = new UploadIdentifier();
-                result.Identifier = guid;
+                result = new UploadIdentifier(guid);
             }
             catch (Exception ex)
             {
@@ -133,5 +150,49 @@ namespace ProxyAPI.Controllers
             return Ok(result);
         }
         #endregion
+
+
+        private void SyncCopyFullPlainTextFileToFile(IFormFile decklist, string decklistFilePath)
+        {
+            /*
+             * NOTE: this function is no longer used, but was the original strategy used by this API.
+             *  I am keeping a copy of it mostly for reference, although I do not suggest using this strategy for safety concerns.
+             *  This is the most basic way to copy a plain text file from the request body to a new file created based on the specified pathing.
+             */
+
+            // Create a file, then copy the file contents from the request body IFormFile to the server synchronously.
+            using (FileStream fileStream = new FileStream(decklistFilePath, FileMode.Create))
+            {
+                decklist.CopyTo(fileStream);
+            }
+        }
+
+        private bool SyncCopyValidPlainTextFileToFile(IFormFile decklist, string decklistFilePath)
+        {
+            bool valid = false;
+
+            string line = null;
+            using (Stream stream = decklist.OpenReadStream())
+            using (StreamReader reader = new StreamReader(stream))
+            using (StreamWriter writer = new StreamWriter(decklistFilePath))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(line) && !string.IsNullOrWhiteSpace(line))
+                    {
+                        string trimmedLine = line.Trim();
+
+                        // Check to see if the first character of the current plain text line is a digit.
+                        if (!string.IsNullOrWhiteSpace(trimmedLine) && char.IsDigit(trimmedLine[0]))
+                        {
+                            writer.WriteLine(trimmedLine);
+                            valid = true;
+                        }
+                    }
+                }
+            }
+
+            return valid;
+        }
     }
 }
--- ProxyAPI/ProxyAPI/Models/UploadIdentifier.cs
+++ ProxyAPI/ProxyAPI/Models/UploadIdentifier.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 namespace ProxyAPI.Models
 {
     /// <summary>
-    /// An identifier for an uploaded decklist.
+    /// A unique identifier associated w/a decklist that has been uploaded.
     /// </summary>
     public class UploadIdentifier
     {
         /// <summary>
-        /// Unique card name.
+        /// Unique identifier.
         /// </summary>
         /// <example>"7643c6b9-8d7f-40ac-a327-81e166e12a9a"</example>
-        public string Identifier;
+        public string Identifier { get; set; }
+
+        public UploadIdentifier(string id)
+        {
+            Identifier = id;
+        }
     }
 }
